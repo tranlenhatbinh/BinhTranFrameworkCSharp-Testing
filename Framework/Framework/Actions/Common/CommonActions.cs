deda52346509106e8c795cf872910935ba424919
tranlenhatbinh <tranlenhatbinh191988@gmail.com>
2017-02-23 00:12:04 +0700

    1

--- Framework/Framework/Actions/Common/CommonActions.cs
+++ Framework/Framework/Actions/Common/CommonActions.cs
@@ -9,10 +9,14 @@ namespace Framework.Actions.Common
     {
         
         public static void GoToUrl()
-        { 
-           // Constant.webdriver = new InternetExplorerDriver();
-            Constant.webdriver = new FirefoxDriver();
+        {
+            // Constant.webdriver = new InternetExplorerDriver();
+
+            FirefoxDriverService service = FirefoxDriverService.CreateDefaultService(@"E:\geckodriver-v0.11.1-win64", "geckodriver.exe");
+            service.FirefoxBinaryPath = @"C:\Program Files (x86)\Mozilla Firefox\firefox.exe";
+            Constant.webdriver = new FirefoxDriver(service);
             Constant.webdriver.Navigate().GoToUrl("http://192.168.1.11");
+            Constant.webdriver.Manage().Window.Maximize();
         }
     }
 }
