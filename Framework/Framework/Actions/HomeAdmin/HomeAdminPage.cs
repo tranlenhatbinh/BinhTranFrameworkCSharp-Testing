a62462e8cfa62f7cdcf9315b73917dbac9735de1
Administrator <Administrator@ADMINISTRATOR>
2017-03-09 22:34:58 +0700

    1

--- Framework/Framework/Actions/HomeAdmin/HomeAdminPage.cs
+++ Framework/Framework/Actions/HomeAdmin/HomeAdminPage.cs
@@ -12,25 +12,25 @@ namespace Framework.Actions.HomeAdmin
 
         public string getOrdersName()
         {
-            WaitForControl("orders link", 5);
+            WaitForControl("orders link", 3);
             return GetText("orders link");
         }
 
         public string getOrdersLinkName()
         {
-            WaitForControl("orders list link", 5);
+            WaitForControl("orders list link", 3);
             return GetText("orders list link");
         }
 
         public void clickOrderListLink()
         {
-            WaitForControl("orders list link", 5);
+            WaitForControl("orders list link", 3);
             Click("orders list link");
         }
 
         public string getOrdersTitleName()
         {
-            WaitForControl("orders title", 5);
+            WaitForControl("orders title", 3);
             return GetText("orders title");
         }
     }
